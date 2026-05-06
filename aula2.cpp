df4547fc6595767a7fffb8bb53f483bfe5275e01
Erica Tatiana <143865619+ericatatiana@users.noreply.github.com>
2026-05-06 09:03:44 +0200

    Delete aula2.cpp

--- aula2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-using namespace std;
-int main() {
-  string nome = "Pessoa"; 
-  int idade = 21; 
-  float altura = 1.65;
-  cout << "Nome: " << nome << endl; cout << "Idade: " << idade << endl; 
-  cout << "Altura: " << altura << endl; return 0;
-}
